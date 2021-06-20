4bf05eac2e40cca9502fa7b5f1edfd18d0c6db64
Igor Abade V. Leite <igor.abade@cloudmotion.com.br>
2021-06-20 17:41:35 -0300

    Update default lifecycle

--- aspnetcore-webapi/template/Controllers/WeatherForecastController.cs
+++ aspnetcore-webapi/template/Controllers/WeatherForecastController.cs
@@ -37,3 +37,17 @@ namespace {{ cookiecutter.namespace }}.Controllers
         }
     }
 }
+
+namespace {{ cookiecutter.namespace }}
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
